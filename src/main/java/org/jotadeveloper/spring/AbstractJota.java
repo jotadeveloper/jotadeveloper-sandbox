d06e175597ba06ebc896ed7d86416b16c1d6ccd7
Picado, Juan <juan@jotadeveloper.com>
2011-01-15 05:39:21 -0600

    Mas cambios probando

--- src/main/java/org/jotadeveloper/spring/AbstractJota.java
+++ src/main/java/org/jotadeveloper/spring/AbstractJota.java
@@ -1,8 +1,13 @@
 package org.jotadeveloper.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public abstract class AbstractJota {
 
      private String name = "YOOOOOOOOO";
+
+     @Autowired
+     private JuanSpring juanSpring;
 
     /**
      * @return the name
@@ -18,4 +23,17 @@ public abstract class AbstractJota {
         this.name = name;
     }
 
+    /**
+     * @return the juanSpring
+     */
+    public JuanSpring getJuanSpring() {
+        return juanSpring;
+    }
+
+    /**
+     * @param juanSpring the juanSpring to set
+     */
+    public void setJuanSpring(JuanSpring juanSpring) {
+        this.juanSpring = juanSpring;
+    }
 }
